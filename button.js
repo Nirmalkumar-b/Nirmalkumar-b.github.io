d337fea43b7b56d1f219e924ebf2c95dce07711d
Nirmalkumar-b <nirmalkumar.b@gaeaglobal.com>
2022-11-23 15:12:15 +0530

    First Website

---
+++ button.js
@@ -0,0 +1,27 @@
+img="gympic1.jpg";
+wid=100;
+hei=100;
+
+let i1= "<img src="+img+" width="+wid+" height="+hei+">";
+
+var price=250;
+
+
+function myFunction()
+ {
+ 
+ 
+  alert("Product Has been added to the cart");
+ 
+  document.write(i1);
+  document.write("<br>Dumbless Rs.250.00");
+  
+  document.write("<br>Total price-"+price);
+  
+   
+
+ }
+
+ 
+ 
+  
